5d6843b9e04ab158f7ad3e3c5486171954e1bb0f
Erick Fuentes <fuentes.erick@gmail.com>
2022-04-09 13:14:20 -0400

    basic implementation of bash

---
+++ sim/cards/bash.hh
@@ -0,0 +1,10 @@
+
+#include "sim/cards/strike.hh"
+
+namespace sts::sim::cards {
+struct Bash : public Strike {
+  Bash(int energy_cost, int damage_amount)
+      : Card("Bash", energy_cost, get_unique_id(), Type::ATTACK),
+        damage_amount(damage_amount) {}
+};
+}  // namespace sts::sim::cards
